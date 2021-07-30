5c28fb073383eea7faff235b60d1781ba5f167ed
Stacia000 <87893462+Stacia000@users.noreply.github.com>
2021-07-30 22:34:26 +0800

    new features
    新增ping指令
    使用json設定檔

--- Bot.py
+++ Bot.py
@@ -1,6 +1,13 @@
 import discord
 #import nest_asyncio
 from discord.ext import commands
+import json
+
+#讀取設定檔
+with open('setting.json','r',encoding='utf8') as jfile: #(檔案，r讀取，編碼)
+    jdata=json.load(jfile)
+
+
 
 #intents設置
 intents=discord.Intents.default() #all.none.default() ex.default except presences&members
@@ -12,7 +19,7 @@ bot=commands.Bot(command_prefix='[',intents=intents)
 #啟動顯示訊息
 @bot.event
 async def on_ready():
-    print(">>Bot is online<<")
+    print(">>阿干肝甘乾Bot is online<<")
 
 #成員加入訊息
 @bot.event
@@ -28,5 +35,10 @@ async def on_member_remove(member):
     await channel.send(f'{member}leave!')
     #print(f'{member}leave!')
 
+#指令
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')#round:小數點後四捨五入/latency:回傳延遲
+
 #執行bot
-bot.run('NzEzMDMwMzM1ODMwNjg3ODU0.XsaLPw.nY9uTZC5_qym7k8ROcuvVbmCE4I')
+bot.run(jdata['TOKEN']) #bot.run(Bot_Token)
